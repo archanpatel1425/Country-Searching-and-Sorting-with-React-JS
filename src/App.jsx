cd6d19dbc6a0abf9e2a04ea1b36a036131f5c94d
bhargavpatelinfo <151021365+bhargavpatelinfo@users.noreply.github.com>
2025-01-16 15:56:53 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Country from './pages/Country'
 import Country_Region from './pages/Country_Region'
@@ -6,21 +6,6 @@ import HomePage from './pages/HomePage'
 import Region from './pages/Region'
 const App = () => {
 
-  useEffect(() => {
-    const handlePageLoad = () => {
-      const script = document.createElement('script');
-      script.src = 'https://script.crazyegg.com/pages/scripts/0127/9523.js'; 
-      script.async = true;
-      document.body.appendChild(script);
-    };
-  
-    window.addEventListener('load', handlePageLoad);
-  
-    return () => {
-      window.removeEventListener('load', handlePageLoad);
-    };
-  }, []);
-  
   return (
     <div>
         <Router>
