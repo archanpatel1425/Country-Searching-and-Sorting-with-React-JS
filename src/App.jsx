965cb091d5f145b8f99c0ba6ac9ad569fff9bcc0
bhargavpatelinfo <151021365+bhargavpatelinfo@users.noreply.github.com>
2025-01-16 14:52:39 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
-import HomePage from './pages/HomePage'
 import Country from './pages/Country'
-import Region from './pages/Region'
 import Country_Region from './pages/Country_Region'
+import HomePage from './pages/HomePage'
+import Region from './pages/Region'
 const App = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://script.crazyegg.com/pages/scripts/0127/9523.js'; // replace XXXXX with your Crazy Egg ID
+    script.async = false; // Load synchronously to prevent FOUC (flash of unstyled content)
+    script.onload = () => {
+      console.log('Crazy Egg script loaded');
+    };
+
+    document.head.appendChild(script);
+
+    return () => {
+      document.head.removeChild(script); // Cleanup script when component unmounts
+    };
+  }, []);
   return (
     <div>
       <Router>
